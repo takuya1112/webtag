83614d81e2d54d512588e83aa1e8ff88e9b8fa9e
Takuya Muraoka <muraoka1112@icloud.com>
2026-04-07 23:15:16 +0900

    add article interface

--- backend/article/domain/repository.py
+++ backend/article/domain/repository.py
@@ -1,6 +1,7 @@
 from typing import Protocol, runtime_checkable
 
 from .entity import ArticleEntity
+from .value_objects import ArticleId
 
 
 @runtime_checkable
@@ -21,9 +22,50 @@ class ArticleRepository(Protocol):
             article (ArticleEntity): The article to soft delete
         """
 
+    def soft_delete_all(self) -> None:
+        """Soft delete all article"""
+
+    def search_by_keywords(self, keywords: list[str]) -> list[ArticleEntity]:
+        """Search articles by keywords"""
+
+    def find_by_id(self, article_id: ArticleId) -> ArticleEntity | None:
+        """Find a article by article id"""
+
+    def find_all(self) -> list[ArticleEntity]:
+        """Find all article"""
+
     def update(self, article: ArticleEntity) -> None:
         """Update a article
 
         Args:
             article (ArticleEntity): The article to update
         """
+
+    def restore(self, article: ArticleEntity) -> None:
+        """Restore a deleted article
+
+        Args:
+            article (ArticleEntity): The deleted article to restore
+        """
+
+    def restore_all(self) -> None:
+        """Restore all deleted article"""
+
+    def hard_delete_deleted(self, article: ArticleEntity) -> None:
+        """Hard delete a deleted article
+
+        Args:
+            article (ArticleEntity): The deleted article to hard delete
+        """
+
+    def hard_delete_all_deleted(self) -> None:
+        """Hard delete all deleted article"""
+
+    def find_deleted_by_id(self, article_id: ArticleId) -> ArticleEntity | None:
+        """Find a deleted article by article id"""
+
+    def find_all_deleted(self) -> list[ArticleEntity]:
+        """Find all deleted article"""
+
+    def delete_outdated_articles(self) -> None:
+        """Delete outdated deleted articles"""
